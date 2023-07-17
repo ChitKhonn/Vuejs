d36826f321779c5a0f92ab4e3c5fbb9987901b92
ckc <chitkhonncho9@gmail.com>
2023-07-17 23:06:26 +0630

    fix component tesing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <h1>{{text}}</h1>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -8,6 +9,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      text:"hey what's up? bro"
+    }
+  },
   components: {
     HelloWorld
   }
